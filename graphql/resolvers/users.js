93f182f405766ae280a276a9c73938e953aec6c6
hari <hari.nair634@gmail.com>
2021-08-07 13:23:06 +0530

    featureadd

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -6,7 +6,7 @@ const User = require('../../models/User');
 
 module.exports = {
     Mutation: {
-        async register(_, {registerInput: {username, email, password, confirmPassword}},context,info){
+        async register(_, {registerInput: { username, email, password, confirmPassword }},context,info){
             // TODO Validate user data
             // TODO Make sure user doesnt already exist
             // TODO hash password and create an auth token
